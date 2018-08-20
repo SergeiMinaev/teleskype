b53c87a35aababf29e24902f60a9c0141dee1e9d
SergeiMinaev <brainsburn@yandex.ru>
2018-08-20 04:10:03 +0300

    Add basic skype echo-bot

---
+++ skype_connector.py
@@ -0,0 +1,53 @@
+import configparser
+import os
+import threading
+from datetime import datetime
+from time import sleep
+from skpy import Skype, SkypeAuthException, SkypeEventLoop
+
+TOKEN_FILE="skype_token"
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+class MySkype(SkypeEventLoop):
+    def onEvent(self, event):
+        if event.type == 'NewMessage' and type(event).__name__ == 'SkypeNewMessageEvent':
+            # ignore self
+            if event.msg.user.id != self.user.id:
+                event.msg.chat.sendMsg(event.msg.content)
+
+
+def check_token_loop(conn):
+    threading.Timer(300, check_token_loop, [conn]).start()
+    token_t = conn.tokenExpiry['skype'].timestamp()
+    now_t = datetime.now().timestamp()
+    if token_t - now_t < 600:
+        print("Trying to refresh skype token...")
+        try:
+            conn.refreshSkypeToken()
+        except SkypeAuthException as e:
+            print("Can't refresh skype token. Login request is rejected",
+                    repr(e))
+        except SkypeApiException as e:
+            print("Can't refresh skype token. Login form can't be processed",
+                    repr(e))
+        else:
+            print("Skype token has been refreshed successfully")
+
+def run():
+    print("Skype connector is running")
+    sk = Skype(connect=False)
+    sk.conn.setTokenFile(TOKEN_FILE)
+    try:
+        sk.conn.readToken()
+    except SkypeAuthException:
+        sk.conn.setUserPwd(config['main']['skype_login'],
+                config['main']['skype_password'])
+        sk.conn.getSkypeToken()
+
+    sk_loop = MySkype(tokenFile = TOKEN_FILE, autoAck = True)
+    sk_loop.loop()
+
+    check_token_loop(sk.conn)
--- teleskype.py
+++ teleskype.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import signal
 import sys
+from threading import Thread
 import telegram_connector
-
+import skype_connector
 
 def ctrl_c_handler(sig, frame):
     sys.exit(0)
@@ -10,4 +11,5 @@ def ctrl_c_handler(sig, frame):
 signal.signal(signal.SIGINT, ctrl_c_handler)
 
 
-telegram_connector.run()
+skype_thread = Thread(target = skype_connector.run).start()
+telegram_thread = Thread(target = telegram_connector.run).start()
